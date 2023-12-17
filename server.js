6ca627e11cf39f5b276efe9d5e4430d0664bf99d
Mohamed1492 <sellami.med.tn@gmail.com>
2023-12-17 10:06:18 +0100

    uploading backend

--- server.js
+++ server.js
@@ -11,4 +11,8 @@ app.use("/user",user);
 // product link
 app.use("/product",product)
 // app.use("/admin",admin);
+
+app.use((req,res)=>{
+    res.send("API is running...")
+})
 app.listen(5000, err=> err?console.log(err):console.log(`the server is running successfully on PORT: 5000`));
